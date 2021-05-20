7f02156187043d2ed08b1405a7ec5e2a552ed44b
Anthony Dunford <trashzy21@gmail.com>
2021-05-20 12:39:54 +0100

    Doors

---
+++ FSharpLearning/ScriptLearning/Doors.fsx
@@ -0,0 +1,20 @@
+﻿
+
+let toggleDoorsModulo m doors =
+    doors
+    |> List.map (fun (n, d) ->
+        match n % m with 
+        | 0 -> (n, not d)
+        | _ -> (n, d)
+    )
+
+let rec toggleDoors doors i =
+    match i with
+    | 0 -> doors
+    | _ -> toggleDoors (toggleDoorsModulo i doors) (i-1)
+
+let doors = [for i in 1..100 -> (i, false)]
+
+let toggledDoors = toggleDoors doors 100   
+
+printf "%A" toggledDoors
