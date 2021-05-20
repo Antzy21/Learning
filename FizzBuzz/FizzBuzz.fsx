7aba73e6a8874c86ec880589cb76a1eb2893f414
Anthony Dunford <trashzy21@gmail.com>
2021-05-20 12:08:46 +0100

    FizzBuzz

---
+++ FizzBuzz/FizzBuzz.fsx
@@ -0,0 +1,26 @@
+﻿
+let isWord modulus (word:string) (x, s) =
+    match x % modulus = 0 with
+    | true -> (x, $"{s}{word}")
+    | false -> (x, s)
+
+let isFizz = isWord 3 "Fizz"
+let isBuzz = isWord 5 "Buzz"
+
+let fillBlanks (x,s) =
+    match s with
+    | "" -> (x,$"{x}")
+    | _ -> (x, s)
+
+let FizzBuzz nums = 
+    nums 
+    |> List.map isFizz
+    |> List.map isBuzz
+    |> List.map fillBlanks
+    |> List.map (fun (x,s) -> s)
+
+let nums = [for i in [1..50] -> (i, "")]
+
+let result = FizzBuzz nums 
+
+printf "%A" result
