e963f8796a58a64642442e35fe557cf5b70a3796
Anthony Dunford <30522408+Antzy21@users.noreply.github.com>
2022-10-16 11:37:29 +0100

    Rename variables

--- FSharpLearning/ScriptLearning/FizzBuzz.fsx
+++ FSharpLearning/ScriptLearning/FizzBuzz.fsx
@@ -1,23 +1,23 @@
 ﻿
-let isWordModulo modulus (word:string) (x, s) =
-    match x % modulus = 0 with
-    | true -> (x, $"{s}{word}")
-    | false -> (x, s)
+let isWordModulo (m: int) (word:string) (n, s) =
+    match n % m = 0 with
+    | true -> (n, $"{s}{word}")
+    | false -> (n, s)
 
 let isFizz = isWordModulo 3 "Fizz"
 let isBuzz = isWordModulo 5 "Buzz"
 
-let fillBlanks (x,s) =
+let fillBlanks (n,s) =
     match s with
-    | "" -> (x,$"{x}")
-    | _ -> (x, s)
+    | "" -> (n,$"{n}")
+    | _ -> (n, s)
 
 let FizzBuzz nums = 
     nums 
     |> List.map isFizz
     |> List.map isBuzz
     |> List.map fillBlanks
-    |> List.map (fun (x,s) -> s)
+    |> List.map (fun (n,s) -> s)
 
 let nums = [for i in [1..50] -> (i, "")]
 
