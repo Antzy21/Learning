642e01fd7282e014bf0085c614b303dad38b354e
Anthony Dunford <trashzy21@gmail.com>
2023-11-10 21:47:37 +0000

    wip

--- Blazer/BlazorApp1/BlazorApp1/Server/Controllers/WeatherForecastController.cs
+++ Blazer/BlazorApp1/BlazorApp1/Server/Controllers/WeatherForecastController.cs
@@ -13,8 +13,8 @@ namespace BlazorApp1.Server.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
